3e41d15c579b0adc96232d4d8ec522f4be0000a0
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-05-24 12:38:08 +0300

    CREATE: Candidate model

---
+++ Models/Candidate.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BtkCamp.Models
+{
+    public class Candidate
+    {
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+    }
+}
